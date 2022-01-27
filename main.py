d5f83dec3f5261ae4c0bb92b73825b3e03fe5f0b
Aviksaikat <saikickkarma@gmail.com>
2022-01-27 21:43:29 +0530

    updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, send_from_directory
 import os
 from io import BytesIO
 # from pylint import pyreverse
@@ -14,33 +14,34 @@ def hello():
 
 @app.route("/upload", methods=['POST'])
 def upload():
-    try:
-        file = request.files['path']
-        split_tup = os.path.splitext(file.filename)
-        if split_tup[1] == '.py':
-            temp = "temp.py"
-            with open(temp, 'wb') as f:
-                f.write(BytesIO(file.read()).read())
-                f.close()
-            path = os.path.abspath(temp)
-            os.system("pyreverse -o png " + path)
-            output = "classes.png"
-            os.remove(temp)
-            return send_file(output, download_name="Uml_diagram.png", as_attachment=True)
-        elif split_tup[1] == '.java':
-            temp = "test.java"
-            with open(temp, 'wb') as f:
-                f.write(BytesIO(file.read()).read())
-                f.close()
-            path = os.path.abspath(temp)
-            os.system("pyreverse -o png " + path)
-            output = "classes.png"
-            os.remove(temp)
-            return send_file(output, download_name="Uml_diagram.png", as_attachment=True)
-        else:
-            print("error")
-    except:
-        return "<h1>Please select a valid file</h1>"
+    #try:
+    file = request.files['path']
+    split_tup = os.path.splitext(file.filename)
+    if split_tup[1] == '.py':
+        temp = "temp.py"
+        with open(temp, 'wb') as f:
+            f.write(BytesIO(file.read()).read())
+            f.close()
+        path = os.path.abspath(temp)
+        #print(f"!!!!!!{path}")
+        os.system("pyreverse -o png " + path)
+        output = "classes.png"
+        os.remove(temp)
+        return send_file(output, download_name="Uml_diagram.png", as_attachment=True)
+    elif split_tup[1] == '.java':
+        temp = "test.java"
+        with open(temp, 'wb') as f:
+            f.write(BytesIO(file.read()).read())
+            f.close()
+        path = os.path.abspath(temp)
+        os.system("pyreverse -o png " + path)
+        output = os.path.abspath("classes.png")
+        os.remove(temp)
+        return send_file(output, download_name="Uml_diagram.png", as_attachment=True)
+    else:
+        print("error")
+    # except:
+    #     return "<h1>Please select a valid file</h1>"
 
 
 @app.route("/upload_mul", methods=['POST'])
@@ -49,14 +50,19 @@ def mul():
         files = request.files.getlist('mul')
         temp = "temp.py"
         for f in files:
-            print(f)
+            #print(f)
             with open(temp, 'ab') as file:
                 file.write(BytesIO(f.read()).read())
                 file.close()
         path = os.path.abspath(temp)
-        os.system("pyreverse -o png " + path)
-        output = "classes.png"
-        os.remove(temp)
+        try:
+            os.system("pyreverse -o png " + path)
+            output = os.path.abspath("classes.png")
+            os.remove(temp)
+        except:
+            return "<h1>bbaba re !!</h1>"
+            #exit(-1)
+        
         return send_file(output, download_name="Uml_diagram.png", as_attachment=True)
 
     except:
